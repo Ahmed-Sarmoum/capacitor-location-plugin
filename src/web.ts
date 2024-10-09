2c908d24da03b754525ec23fb58e65ac9860f7ae
Ahmed-Sarmoum <hamadadevloper93@gmail.com>
2024-10-09 21:49:06 +0100

    🔥 Fix issue

--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,8 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { LocationPluginPlugin } from './definitions';
+import type { LocationPlugin } from './definitions';
 
-export class LocationPluginWeb extends WebPlugin implements LocationPluginPlugin {
+export class LocationPluginWeb extends WebPlugin implements LocationPlugin {
   async isLocationEnabled(): Promise<{ isEnabled: boolean }> {
     console.log('Checking location enabled status...');
     // For the web, you can default to false, or implement actual checks if needed.
